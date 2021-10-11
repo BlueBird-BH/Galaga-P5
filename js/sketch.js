2a93598211b861924407d1335a31735f121b0d2a
Blue Bird <52676679+BlueBird-BH@users.noreply.github.com>
2021-10-11 15:03:16 +0000

    Se agregaron valores para probar que el archivo se ejecutara correctamente en Replit

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,50 @@
+var centroX, centroY;
+
+function preload() {
+	imagen = "media/estrellas.jpg";
+	espacioExterior = loadImage(imagen);
+}
+
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(700, 700);
+	centroX = width / 2;
+	centroY = height / 2;
+
+	sol = new Planeta((aspecto = "#f5cd07"), (tamaño = 70), (orbita = 0));
 }
 
 function draw() {
-  background(0);
+	background(espacioExterior);
+
+	sol.fijar();
+}
+
+class Planeta {
+	constructor(aspecto, tamaño, orbita) {
+		this.aspecto = aspecto;
+		this.velocidad = 27 / (tamaño * orbita * 2);
+		this.tamaño = tamaño;
+
+		this.anguloInicial = 0;
+		this.orbita = orbita;
+		this.x = centroX;
+		this.y = centroY;
+	}
+
+	angulo() {
+		return (this.anguloInicial += this.velocidad);
+	}
+
+	fijar() {
+		noStroke();
+		fill(this.aspecto);
+		ellipse(this.x, this.y, this.tamaño);
+	}
+
+	orbitar() {
+		this.x = centroX + this.orbita * cos(this.angulo());
+		this.y = centroX + this.orbita * sin(this.angulo());
+
+		this.fijar();
+	}
 }
